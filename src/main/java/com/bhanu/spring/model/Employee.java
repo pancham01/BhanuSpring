a7e0e1c013ad6d1a6418c0373214f6333c8afb17
pancham01 <pancham9557@gmail.com>
2025-11-11 12:11:18 +0530

    update

--- src/main/java/com/bhanu/spring/model/Employee.java
+++ src/main/java/com/bhanu/spring/model/Employee.java
@@ -1,39 +1,72 @@
 package com.bhanu.spring.model;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Employee {
 
 //	Instance var / attributes / Properties 
 	private String name, gender;
 	private int id;
-	
+
 	private Address address;
-	
+
+	List<Integer> list;
+
+	Set<Integer> set;
+
+	Map<Integer, String> map;
 
 	public Employee() {
 		super();
 		System.out.println("Employee.Employee()");
 	}
 
-	
-	public Employee(String name, String gender, int id, Address address) {
+	public Employee(String name, String gender, int id, Address address, List<Integer> list, Set<Integer> set,
+			Map<Integer, String> map) {
 		super();
 		this.name = name;
 		this.gender = gender;
 		this.id = id;
 		this.address = address;
+		this.list = list;
+		this.set = set;
+		this.map = map;
 	}
 
+	public List<Integer> getList() {
+		return list;
+	}
+
+	public void setList(List<Integer> list) {
+		this.list = list;
+	}
+
+	public Set<Integer> getSet() {
+		return set;
+	}
+
+	public void setSet(Set<Integer> set) {
+		this.set = set;
+	}
+
+	public Map<Integer, String> getMap() {
+		return map;
+	}
+
+	public void setMap(Map<Integer, String> map) {
+		this.map = map;
+	}
 
 	public Address getAddress() {
 		return address;
 	}
 
-
 	public void setAddress(Address address) {
 		this.address = address;
 		System.out.println("Employee.setAddress()");
 	}
-
 
 	public String getName() {
 		return name;
@@ -62,11 +95,10 @@ public class Employee {
 		System.out.println("Employee.setId()");
 	}
 
-
 	@Override
 	public String toString() {
-		return "Employee [name=" + name + ", gender=" + gender + ", id=" + id + ", address=" + address + "]";
+		return "Employee [name=" + name + ", gender=" + gender + ", id=" + id + ", address=" + address + ", list="
+				+ list + ", set=" + set + ", map=" + map + "]";
 	}
 
-	
 }
